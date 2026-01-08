56c4ca71f7ed8e99355638815f6058ba33578bd8
rate0621 <chat.rate0621@gmail.com>
2026-01-08 21:03:42 +0900

    Initial Commit

---
+++ swipe-arrows/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  swipe-arrows
+//
+//  Created by rate on 2026/01/08.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ swipe-arrows/swipe_arrowsApp.swift
@@ -0,0 +1,17 @@
+//
+//  swipe_arrowsApp.swift
+//  swipe-arrows
+//
+//  Created by rate on 2026/01/08.
+//
+
+import SwiftUI
+
+@main
+struct swipe_arrowsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
